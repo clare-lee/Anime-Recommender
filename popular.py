8b86bc6417346d839c3f763a6c98746b40022ecc
clare-lee <56900294+clare-lee@users.noreply.github.com>
2022-04-06 16:07:37 -0400

    added pages

---
+++ popular.py
@@ -0,0 +1,15 @@
+import pandas as pd
+from sqlalchemy import create_engine
+
+engine = create_engine('mysql+pymysql://root@localhost:3306/Recommender')
+query = engine.execute('SELECT * FROM anime')
+
+rows = query.fetchall()
+anime = pd.DataFrame(rows)
+
+anime_tv = anime.loc[anime['medium'] == 'TV']
+anime_tv = anime_tv.sort_values(by='rating', ascending=False)
+anime_tv.reset_index(drop=True, inplace=True)
+
+
+print(anime_tv.head(10))
--- website/views.py
+++ website/views.py
@@ -1,18 +1,33 @@
 from flask import Blueprint, render_template
 from .models import Anime
 from flask_login import login_required, current_user
+import pandas as pd
+from sqlalchemy import create_engine
 views = Blueprint('views', __name__)
 
 
 @views.route('/')
 def home():     # this function will run whenever we go to route
-    return render_template("home.html")
+    engine = create_engine('mysql+pymysql://root@localhost:3306/Recommender')
+    query = engine.execute('SELECT * FROM anime')
+
+    rows = query.fetchall()
+    anime = pd.DataFrame(rows)
+
+    # The highiest rated TV-series
+    anime_tv = anime.loc[anime['medium'] == 'TV']
+    anime_tv = anime_tv.sort_values(by='rating', ascending=False)
+    anime_tv.reset_index(drop=True, inplace=True)
+    tvs = anime_tv.head(10)
+    print(tvs)
+    return render_template("home.html", tvs = tvs)
     
 @views.route('/anime')
 def anime_view():
     print(Anime.__table__)
     animelist = Anime.query.all()
     return render_template("anime.html", animelist = animelist)
+
 
 # @views.route('/recommendation')
 # def anime_view():
