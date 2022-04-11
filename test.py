611a41af680e47f0b8ca0817a2e1c72a9abec34f
clare-lee <56900294+clare-lee@users.noreply.github.com>
2022-04-10 22:04:41 -0400

    genre array list function

--- test.py
+++ test.py
@@ -1,12 +1,66 @@
 import pandas as pd
 from sqlalchemy import create_engine
 
-engine = create_engine('mysql+pymysql://root@localhost:3306/Recommender')
-query = engine.execute('SELECT * FROM anime')
+# engine = create_engine('mysql+pymysql://root@localhost:3306/Recommender')
+# query = engine.execute('SELECT * FROM anime')
 
-rows = query.fetchall()
-anime = pd.DataFrame(rows)
+# rows = query.fetchall()
+# anime = pd.DataFrame(rows)
 
+import numpy as np
 
+picked_genres = np.zeros(43)
+
+anime_genres = ['Action',
+ 'Adventure',
+ 'Comedy',
+ 'Drama',
+ 'Sci-Fi',
+ 'Space',
+ 'Mystery',
+ 'Magic',
+ 'Police',
+ 'Supernatural',
+ 'Fantasy',
+ 'Shounen',
+ 'Sports',
+ 'Josei',
+ 'Romance',
+ 'Slice of Life',
+ 'Cars',
+ 'Seinen',
+ 'Horror',
+ 'Psychological',
+ 'Thriller',
+ 'Martial Arts',
+ 'Super Power',
+ 'School',
+ 'Ecchi',
+ 'Vampire',
+ 'Historical',
+ 'Military',
+ 'Dementia',
+ 'Mecha',
+ 'Demons',
+ 'Samurai',
+ 'Game',
+ 'Shoujo',
+ 'Harem',
+ 'Music',
+ 'Shoujo Ai',
+ 'Shounen Ai',
+ 'Kids',
+ 'Hentai',
+ 'Parody',
+ 'Yuri',
+ 'Yaoi']
+
+user_list = ['Hentai', 'Yaoi']
+
+print(len(anime_genres))
+for g in user_list:
+    for i in range(len(anime_genres)):
+        if g == anime_genres[i]:
+            picked_genres[i] = 1
             
-print(anime)
+print(picked_genres)
